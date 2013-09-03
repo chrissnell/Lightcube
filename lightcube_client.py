05357db84e5376e58819afd3db7fa4dc066ca040
Christopher Snell <chris.snell@gmail.com>
2013-09-03 16:20:33 -0500

    Clean-up of example client

--- lightcube_client.py
+++ lightcube_client.py
@@ -1,13 +1,13 @@
 import Lightcube
 
 # Define some generic colors
-RED = Lightcube.Color(rgb="880000")
-WHITE = Lightcube.Color(rgb="ffffff")
-BLUE = Lightcube.Color(rgb="0000aa")
-GREEN = Lightcube.Color(rgb="215E21")
-YELLOW = Lightcube.Color(rgb="ffc400")
-BLACK = Lightcube.Color(rgb="000000")
-GREY = Lightcube.Color(rgb="222222")
+RED = Lightcube.Color(rgb=0x880000)
+WHITE = Lightcube.Color(rgb=0xffffff)
+BLUE = Lightcube.Color(rgb=0x0000aa)
+GREEN = Lightcube.Color(rgb=0x215E21)
+YELLOW = Lightcube.Color(rgb=0xffc400)
+BLACK = Lightcube.Color(rgb=0x000000)
+GREY = Lightcube.Color(rgb=0x222222)
 
 
 # Create a new Frame
@@ -15,16 +15,18 @@ myframe = Lightcube.Frame(retain_delay=0xA)
 # and a FrameRenderer
 myrenderer = Lightcube.FrameRenderer(frame=myframe)
 
-# Define the start and end points of the line
-line_start = Lightcube.Coordinate(x=0, y=3)
-line_end = Lightcube.Coordinate(x=7, y=3)
-# and draw a red line between them
-myrenderer.draw_line(line_start, line_end, RED)
 
 # Define the lower-left corner of a box
 box_ll = Lightcube.Coordinate(x=0, y=0)
 # and draw a 9x12 box starting there
-myrenderer.draw_box(box_ll, 9, 12)
+myrenderer.draw_box(box_ll, 9, 12, GREEN)
+
+# Define the start and end points of the line
+line_start = Lightcube.Coordinate(x=0, y=1)
+line_end = Lightcube.Coordinate(x=0, y=7)
+# and draw a red line between them
+myrenderer.draw_line(line_start, line_end, RED)
+
 
 # Create a new assembled frame packet
 packet = Lightcube.AssembledFramePacket(frame=myframe)
