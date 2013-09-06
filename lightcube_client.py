1d37c3bfea4da59d2f9588e1e4060e6c5eba409d
Christopher Snell <chris.snell@gmail.com>
2013-09-06 16:28:15 -0500

    Demo for hackday

--- lightcube_client.py
+++ lightcube_client.py
@@ -1,10 +1,10 @@
 import Lightcube
 
 # Define some generic colors
-RED = Lightcube.Color(rgb=0x880000)
+RED = Lightcube.Color(rgb=0xFF0000)
 WHITE = Lightcube.Color(rgb=0xffffff)
-BLUE = Lightcube.Color(rgb=0x0000aa)
-GREEN = Lightcube.Color(rgb=0x215E21)
+BLUE = Lightcube.Color(rgb=0x0000FF)
+GREEN = Lightcube.Color(rgb=0x00FF00)
 YELLOW = Lightcube.Color(rgb=0xffc400)
 BLACK = Lightcube.Color(rgb=0x000000)
 GREY = Lightcube.Color(rgb=0x222222)
@@ -19,13 +19,35 @@ myrenderer = Lightcube.FrameRenderer(frame=myframe)
 # Define the lower-left corner of a box
 box_ll = Lightcube.Coordinate(x=0, y=0)
 # and draw a 9x12 box starting there
-myrenderer.draw_box(box_ll, 9, 12, GREEN)
+myrenderer.draw_box(box_ll, 4, 4, RED)
 
 # Define the start and end points of the line
-line_start = Lightcube.Coordinate(x=0, y=1)
+line_start = Lightcube.Coordinate(x=0, y=0)
 line_end = Lightcube.Coordinate(x=0, y=7)
 # and draw a red line between them
-myrenderer.draw_line(line_start, line_end, RED)
+myrenderer.draw_line(line_start, line_end, GREEN)
+
+
+myrenderer.draw_line(Lightcube.Coordinate(x=4, y=0), Lightcube.Coordinate(x=4, y=2), BLUE)
+
+myrenderer.draw_line(Lightcube.Coordinate(x=5, y=0), Lightcube.Coordinate(x=5, y=1), BLUE)
+
+myrenderer.draw_line(Lightcube.Coordinate(x=7, y=0), Lightcube.Coordinate(x=2, y=7), YELLOW)
+
+myrenderer.draw_point(Lightcube.Coordinate(x=5, y=5), WHITE)
+myrenderer.draw_point(Lightcube.Coordinate(x=5, y=6), WHITE)
+myrenderer.draw_point(Lightcube.Coordinate(x=5, y=7), WHITE)
+myrenderer.draw_point(Lightcube.Coordinate(x=6, y=7), WHITE)
+myrenderer.draw_point(Lightcube.Coordinate(x=7, y=7), WHITE)
+myrenderer.draw_point(Lightcube.Coordinate(x=7, y=6), WHITE)
+myrenderer.draw_point(Lightcube.Coordinate(x=7, y=5), WHITE)
+myrenderer.draw_point(Lightcube.Coordinate(x=6, y=5), WHITE)
+
+
+
+
+
+
 
 
 # Create a new assembled frame packet
@@ -35,4 +57,4 @@ packet = Lightcube.AssembledFramePacket(frame=myframe)
 packet.create_packet()
 
 # and send it over the wire
-packet.send_packet("127.0.0.1", 7070)
+packet.send_packet("192.168.17.2", 7070)
